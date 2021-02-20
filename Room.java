69a66a10471231ca399f306fd946ba65bfe6a9e9
Michael Spencer <mspencer009@gmail.com>
2021-02-19 20:21:16 -0800

    Created World class

--- Room.java
+++ Room.java
@@ -1,7 +1,18 @@
 public class Room {
     private enum dirs{N, NE, E, SE, S, SW, W, NW, U, D};
     private String description;
+    private 
 
+    private boolean hasN;
+    private boolean hasNE;
+    private boolean hasE;
+    private boolean hasSE;
+    private boolean hasS;
+    private boolean hasSW;
+    private boolean hasW;
+    private boolean hasNW;
+    private boolean hasU;
+    private boolean hasD;
     
 
 }
