1834a48f9e26beedc5fe0f648c1152cbcedf1351
Michael Spencer <mspencer009@gmail.com>
2021-04-28 13:32:26 -0700

    Starting items and inventory

--- Main.java
+++ Main.java
@@ -2,14 +2,10 @@
 
 public class Main{
     public static void main(String[] args){
-    	String[] d1 = {"n"};
-    	
-        Room room1 = new Room("INTERIOR", d1, "Starting room. There is nothing here.");
-        String[] d2 = {"s"};
-        Room room2 = new Room("INTERIOR", room1, d2, "The second room. Congratulations if you made it here!");
-        Player player = new Player(room1);
-        Input input = new Input(player);
-        input.input();
+    	World world = new World();
+        world.createWorld();
+        
+        
         
     }
 }
